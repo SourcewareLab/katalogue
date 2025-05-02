a549a367f7c3d7f9f12ebd1fbf301d52e9f68966
hegedus-mark <hegedusmark2018@gmail.com>
2025-05-02 18:44:43 +0200

    feat: Add layoutWrapper

--- client-workspace/frontend/src/app/app.component.ts
+++ client-workspace/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { LayoutComponent } from "./core/components/layout/layout.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, LayoutComponent],
+  imports: [ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
